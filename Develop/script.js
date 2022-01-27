6e57a869eaf37069f3a4788955c44b1d009ce7fc
Marisol Bennett <marisol_ramirez21@live.com>
2022-01-26 21:05:48 -0600

    changing code around prompts not showing up

--- Develop/script.js
+++ Develop/script.js
@@ -6,21 +6,52 @@ var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var symbols = ["!", "#", "$", "%", "@", "&", "'", "*", "+", "(", ")", "-", ".", "_", ";", "/", ":", ";", "<", "=", ">", "?", "~"];
 
-// question prompts 
-var genBtnPrompt = function () {
+// question prompts for password creation
+function generatePassword() {
   
-  var okayNumbers = confirm("Click 'OK' if you'd like to have generated numbers in your password.");
+  var confirmNumber = confirm("Click 'OK' if you'd like to have generated numbers in your password.");
 
-  var okayLowerCase = confirm("Click 'OK' if you want lowerCase characters to be generated in your password.");
+  var confirmLowerCase = confirm("Click 'OK' if you want lowerCase characters to be generated in your password.");
 
-  var okayUpperCase = confirm("Click 'OK' if you want upperCase characters to be generated in your password.");
+  var confirmUpperCase = confirm("Click 'OK' if you want upperCase characters to be generated in your password.");
 
-  var okaySymbols = confirm("Click 'OK' to generate symbol characters within your password.");
+  var confirmSymbols = confirm("Click 'OK' to generate symbol characters within your password.");
 
-  if (!(okayNumbers || okayLowerCase || okayUpperCase || okaySymbols)) {
-      alert("You need to pick at least one option!");
-      return genBtnPrompt()
+  var passwordLength = parseInt(prompt("How long would you like your password to be? Please pick a number between (8-150)"));
+  
+  var password = "";
+  
+  
+
+  if (!(confirmNumbers || confirmLowerCase || confirmUpperCase || confirmSymbols)) {
+      alert("You need to pick at least one!");
+  };
+
+
+//function generatePassword() {
+  // convert a string to an integer
+  var passwordLength = parseInt(prompt("How long would you like your password to be? Please pick a number between (8-150)"));
+    if (passwordLength < 8 || passwordLength > 150 || passwordLength === "" || isNaN(passwordLength)) {
+      alert("Please choose a number between (8-150)");
+};
+
+var result = "";
+
+
+    // for loop to generate the possibilities
+    for (var i = 0; i < passwordLength; i++) {
+
+        if (confirmNumber === true && confirmLowerCase === true && confirmUpperCase === true && confirmSymbols === true) {
+            result += (allChar[(Math.floor(Math.random() * allChar.length))]);
+        }
+        else if (confirmLowerCase === true && confirmNumber === true) {
+            result += (lowerNum[(Math.floor(Math.random() * lowerNum.length))]);
+        }
+        
   }
+
+    //this returns the value to the password variable declared created below and inserts it into the HTML textarea using the querySelector
+    return result;
 };
 
 // Get references to the #generate element
