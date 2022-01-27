2f6f8853a4b1ecb2f6e330b87cc5ea334b4460f4
Marisol Bennett <marisol_ramirez21@live.com>
2022-01-26 18:24:27 -0600

    setting up questions

--- Develop/script.js
+++ Develop/script.js
@@ -6,6 +6,12 @@ var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var symbols = ["!", "#", "$", "%", "@", "&", "'", "*", "+", "(", ")", "-", ".", "_", ";", "/", ":", ";", "<", "=", ">", "?", "~"];
 
+// question prompts 
+var generateBtnPrompt = function () {
+
+  
+}
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
